5ca9f54a472bc233572eeb8f9087d1162c69f9d5
TheCube\Maxime <maximeflageole@gmail.com>
2019-03-29 21:36:31 -0700

    Added Card and CardFactory scripts, and made the scene UI slightly better

---
+++ Turquoise Flavour/Assets/Scripts/Card.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cards;
+using UnityEngine;
+
+namespace Cards
+{
+    public enum ETarget
+    {
+        Self,
+        Enemy,
+        Creature,
+        Card,
+        None
+    }
+}
+
+public class Card : MonoBehaviour
+{
+    [SerializeField]
+    protected uint m_manaCost = 0;
+    [SerializeField]
+    protected ETarget m_target = ETarget.None;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
