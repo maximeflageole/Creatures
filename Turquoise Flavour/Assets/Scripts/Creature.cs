4290a5dac8bea96906c9dbb5b40f23a629e84fa4
TheCube\Maxime <maximeflageole@gmail.com>
2019-03-29 21:09:51 -0700

    First commit Project bases. A quick UI was done to show how the scene could appear. Nothing functionnal yet.

---
+++ Turquoise Flavour/Assets/Scripts/Creature.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Turquoise;
+using UnityEngine;
+
+namespace Turquoise
+{
+    public enum ETeams
+    {
+        Player,
+        Enemy,
+        Wild,
+        Ally,
+        None
+    }
+}
+
+public class Creature : MonoBehaviour
+{
+    [SerializeField]
+    protected ETeams m_team = ETeams.None;
+    [SerializeField]
+    protected Creatures.CreaturesType m_primaryType;
+    [SerializeField]
+    protected float m_health = 100.0f;
+    [SerializeField]
+    protected float m_maxHealth = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Turquoise Flavour/Assets/Scripts/CreatureType.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Creatures
+{
+    public enum CreaturesType
+    {
+        Dragon,
+        Mineral,
+        Beast,
+        Magic,
+        Plant,
+        Dark,
+        Monster,
+        Automaton,
+        Robotic,
+        Aquatic,
+        Elemental,
+        None
+    };
+}
