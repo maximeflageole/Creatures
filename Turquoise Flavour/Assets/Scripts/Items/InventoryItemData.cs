50c41aac3a13c83af1a3aa050d4c28b034bdd09a
Nixonm <minigol5@hotmail.com>
2019-08-23 16:22:40 -0400

    Added inventoryManager, inventoryItem and inventoryItemData. Added inventoryUI, WIP Fixed a minor loading bug on node generations

---
+++ Turquoise Flavour/Assets/Scripts/Items/InventoryItemData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class InventoryItemData : ScriptableObject
+{
+    public string itemName;
+    public Sprite sprite;
+    public string description;
+    public int cost;
+    public Turquoise.ERarity rarity;
+}
---
+++ Turquoise Flavour/Assets/Scripts/UI/InventoryUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField]
+    protected Color unselectedItemColor;
+    [SerializeField]
+    protected Color selectedItemColor;
+
+    public void SelectTms()
+    {
+
+    }
+
+    public void SelectConsumables()
+    {
+
+    }
+
+    public void SelectOthers()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
