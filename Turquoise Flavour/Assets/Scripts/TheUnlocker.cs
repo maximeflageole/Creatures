19e0e3ad8ed81bd0035b83139ae75cad6db49bd7
Nixonm <minigol5@hotmail.com>
2019-09-16 21:20:17 -0400

    Added unlock data Put that in an update validation. Should probably not be there

--- Turquoise Flavour/Assets/Scripts/TheUnlocker.cs
+++ Turquoise Flavour/Assets/Scripts/TheUnlocker.cs
@@ -7,6 +7,8 @@ public class TheUnlocker : MonoBehaviour
 {
     public static TheUnlocker m_instance;
     public static TheUnlocker GetInstance() { return m_instance; }
+    [SerializeField]
+    protected List<UnlockData> m_unlocksData;
 
     List<EExplorator> m_unlockedExploratorsAtGameEnds = new List<EExplorator>();
 
@@ -55,5 +57,65 @@ public class TheUnlocker : MonoBehaviour
     public void OnGamesEnd()
     {
         UnlockExplorators(m_unlockedExploratorsAtGameEnds);
+    }
+
+    public void Update()
+    {
+        //TODO: Probably not put this in a for loop dummy
+        foreach (var data in m_unlocksData)
+        {
+            bool unlocked = false;
+            int statNumber = StatisticsManager.GetInstance().GetStatAmount(data.stat);
+            if (data.comparisonStat != EStat.Count)
+            {
+                unlocked = ValidateCondition(data.operation, statNumber, StatisticsManager.GetInstance().GetStatAmount(data.comparisonStat));
+            }
+            else
+            {
+                unlocked = ValidateCondition(data.operation, statNumber, data.comparisonNumber);
+            }
+            if (unlocked)
+            {
+                UnlockContent(data);
+            }
+        }
+    }
+
+    public bool ValidateCondition(EOperation operation, int a, int b)
+    {
+        switch (operation)
+        {
+            case EOperation.Bigger:
+                return (a > b);
+                
+            case EOperation.BiggerOrEqual:
+                return (a >= b);
+                
+            case EOperation.Equals:
+                return (a == b);
+                
+            case EOperation.Less:
+                return (a < b);
+                
+            case EOperation.LessOrEqual:
+                return (a <= b);
+                
+            case EOperation.NotEqual:
+                return (a != b);
+        }
+        return false;
+    }
+
+    void UnlockContent(UnlockData data)
+    {
+        UnlockExplorators(data.exploratorsUnlocks);
+        foreach (var unlock in data.cardsUnlocks)
+        {
+            //TODO
+        }
+        foreach (var unlock in data.itemsUnlocks)
+        {
+            //TODO
+        }
     }
 }
---
+++ Turquoise Flavour/Assets/Scripts/Unlocks/UnlockData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Turquoise;
+
+[CreateAssetMenu(fileName = "New Unlock", menuName = "Unlock")]
+public class UnlockData : ScriptableObject
+{
+    public EStat stat;
+    public EOperation operation;
+    public int comparisonNumber;
+    public EStat comparisonStat;
+    public List<EExplorator> exploratorsUnlocks;
+    public List<ECard> cardsUnlocks;
+    public List<EItem> itemsUnlocks;
+}
+
+namespace Turquoise
+{
+    public enum EOperation
+    {
+        Equals,
+        Bigger,
+        Less,
+        BiggerOrEqual,
+        LessOrEqual,
+        NotEqual,
+        Count
+    }
+}
